df8ff42c5fac8a9aa3f247526f8e4b0156c04471
therealmasterchief <flamethegamer26@gmail.com>
2024-12-05 21:39:28 -0400

    menu

--- menu.py
+++ menu.py
@@ -7,11 +7,11 @@ class RestaurantOrder:
         self.window=window
         self.window.title("Restaurant Management App")
 
-        self.menu_items{
+        self.menu_items={
             "FRIES MEAL":2,
             "LUNCH MEAL":2,
             "BURGER MEAL":3,
-            "PIZZA MEAL":4
+            "PIZZA MEAL":4,
             "CHEESE BURGER":2,
             "DRINKS":1
         }
@@ -57,4 +57,36 @@ class RestaurantOrder:
     
     def update_menu_prices(self,*args):
         currency= self.currency_var.get()
-        symbol="₹" if currency
+        symbol="₹" if currency=="INR" else "$"
+        rate=self.exchange_rate if currency == "INR" else 1
+        for item, label in self.menu_labels.items():
+            price=self.menu_items[item]*rate
+            label.config(text=f"{item}({symbol}{price}):")
+        
+    def place_order(self):
+        total_cost=0
+        order_summary="order summary:\n"
+        currency=self.currency_var.get()
+        symbol="₹" if currency=="INR" else "$"
+        rate=self.exchange_rate if currency=="INR" else 1
+        for item, entry in self.menu_quantities.items():
+            quantity=entry.get()
+            if quantity.isdigit():
+                quantity=int(quantity)
+                price=self.menu_items[items]*rate
+                cost=quantity*price
+                total_cost+= cost
+                if quantity>0:
+                    order_summary += f"{item}:{quantity} x{symbol}{price}={symbol}{cost}\n"
+        if total_cost>0:
+            order_summary += f"\nTotal Cost: {symbol}{total_cost}"
+            messagebox.showinfo("Order Placed",order_summary)
+        else:
+            messagebox.showerror("Error","Please Order at least 1 item")
+
+
+if __name__=="__main__ ":
+    window= tk.Tk()
+    app= RestaurantOrder(window)
+    window.geometry("800x600")
+    window.mainloop()
