1cb87336d9ecd425e0be68e68240b5b41e8357f1
therealmasterchief <flamethegamer26@gmail.com>
2024-12-12 21:02:11 -0400

    1 time

---
+++ efficient.py
@@ -0,0 +1,9 @@
+def printnumber(n):
+    iteration=0
+    print("the number entered by user is\n", n)
+    iteration+=1
+    print("total number of iterations doneby this code is:",iteration,"/n")
+
+printnumber(10)
+printnumber(20)
+print("\n With any 'n' the time taken by our code wont change")
